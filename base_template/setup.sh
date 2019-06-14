3f57be017bdbfb56c039a1f39a5495fd227f75d6
= <nichodlofski@gmail.com>
2019-06-13 20:42:29 -0500

    Fix base_template template.json formatting issue

--- base_template/setup.sh
+++ base_template/setup.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 if [ "$1" != "" ]; then
     mkdir -p "$1/files"
-    echo "{\n}" > "$1/template.json"
+    echo -e "{\n}" > "$1/template.json"
     exit 0
 else 
     >&2 echo "sh: template name expected as argument"
