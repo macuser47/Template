5d0392d9ec365426d0b498aebefcbc870c6e8d11
= <nichodlofski@gmail.com>
2019-06-14 15:48:48 -0500

    Make sure template template cleans up folder instaging

--- base_template/setup.sh
+++ base_template/setup.sh
@@ -2,6 +2,8 @@
 if [ "$1" != "" ]; then
     mkdir -p "../../$1/files"
     mv template.json "../../$1"
+    cd ..
+    rmdir template
     exit 0
 else 
     >&2 echo "sh: template name expected as argument"
